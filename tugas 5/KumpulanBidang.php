4906ef6911650d492efd8f68330036901dd026d0
anggiayuningtyas <112960245+anggiayuningtyas@users.noreply.github.com>
2022-10-06 21:41:02 +0700

    Add files via upload

---
+++ tugas 5/KumpulanBidang.php
@@ -0,0 +1,8 @@
+<?php 
+
+    abstract class kumpulanBidang
+    {
+        abstract public function namaBidang();
+        abstract public function luasBidang();
+        abstract public function kelilingBidang();
+    }
